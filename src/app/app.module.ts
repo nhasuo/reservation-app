3c0680d004ad667614ae4c18751554be894f8c13
nhasuo <naosepa0726@gmail.com>
2021-10-01 23:11:57 +0900

    Angularフロントエンド側作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './common/navbar/navbar.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
